e99f07221d519d698d57669d194e3c8329ecb739
Marcos-Teste/QA-SEPLAN <marcossistemas.ac@gmail.com>
2023-11-10 15:56:11 -0500

    ---------

--- templates/index.php
+++ templates/index.php
@@ -11,17 +11,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MovieStar</title>
-    <link rel="short icon" href="img/moviestar.ico">
+    <link rel="short icon" href="<?=$BASE_URL ?> img/moviestar.ico" />
     <!-- BOOTSTRAP -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.2/css/bootstrap.css">
     <!-- FONT-AWESOME -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- CSS do projeto -->
-    <link rel="stylesheet" href="CSS/styles.css"> 
+    <link rel="stylesheet" href="<?=$BASE_URL ?>CSS/styles.css"> 
 </head>
 <body>
 
+    <header>
 
+        <nav id="main-navbar" class="navbar navbar-expand-lg">
+            
+        </nav>
+
+    </header>
     <h1>Testando MovieStar</h1>
     <i class="far fa-plus-square"></i>
     <!-- BOOTSTRAO JS  -->
