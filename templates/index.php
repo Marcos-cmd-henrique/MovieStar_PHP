e8d703944d8b2aa6b742afd0ba4578bbdddfae08
Marcos-Teste/QA-SEPLAN <marcossistemas.ac@gmail.com>
2023-11-10 13:19:54 -0500

    index do projeto iniciada

---
+++ templates/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MovieStar</title>
+    <link rel="short icon" href="img/moviestar.ico" />
+    <!-- CSS do projeto -->
+    <link rel="stylesheet" href="CSS/styles.css"> 
+</head>
+<body>
+    <h1>Testando MovieStar</h1>
+</body>
+</html>
